83a06b2295d3db466718d650720107fe6789f04f
Anna Hyvärinen <anna.hyvarinen@gmail.com>
2017-04-12 13:12:31 +0000

    ADDED SSH FOLDER

---
+++ ssh/manifests/init.pp
@@ -0,0 +1,18 @@
+class ssh {
+	package { ssh:
+		ensure => 'installed',
+		allowcdrom => 'true',
+	}
+	
+	file { '/etc/ssh/sshd_config':
+		content => template('ssh/sshd_config.erb'),
+		notify => Service['ssh'],
+	}
+	
+	service { 'ssh':
+		ensure => 'true',
+		enable => 'true',
+		provider => 'systemd',
+	}
+
+}
